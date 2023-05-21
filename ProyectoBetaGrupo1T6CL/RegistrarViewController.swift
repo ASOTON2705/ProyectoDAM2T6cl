21db47f7ad98b4dc55d07a9d16f17c85c51e7766
JorgeChavezG <jorge10009@hotmail.com>
2023-05-20 22:04:28 -0500

    avances jorge

---
+++ ProyectoBetaGrupo1T6CL/RegistrarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrarViewController.swift
+//  ProyectoBetaGrupo1T6CL
+//
+//  Created by DISEÑO on 20/05/23.
+//
+
+import UIKit
+
+class RegistrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
